c06106d600c184e163708f56b90c0784ecc45075
gregczaplicki <mfkxq8462>
2022-06-28 20:39:07 -0400

    remove example

--- src/pages/api/examples.ts
+++ src/pages/api/examples.ts
@@ -2,7 +2,7 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 import { prisma } from "../../server/db/client";
 
-export default async (req: NextApiRequest, res: NextApiResponse) => {
-  const examples = await prisma.example.findMany();
-  res.status(200).json(examples);
-};
+// export default async (req: NextApiRequest, res: NextApiResponse) => {
+//   const examples = await prisma.example.findMany();
+//   res.status(200).json(examples);
+// };
